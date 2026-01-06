72edc432e8b16f256436f3c600920bc8b79249d6
Lucas <lucas.barbosa.nas@gmail.com>
2026-01-06 13:42:43 -0300

    Adiciona asserção de log aos testes

--- tests/cli/cli.test.ts
+++ tests/cli/cli.test.ts
@@ -11,6 +11,8 @@ describe('CLI interactive', () => {
 	it('uses prompt correctly', async () => {
 		const originalCwd = process.cwd();
 		const tmpProject = await createTmpProject();
+		const mockConsoleLog = vi.fn();
+		console.log = mockConsoleLog;
 
 		(prompts as unknown as Mock).mockResolvedValue({
 			entity: 'User',
@@ -21,6 +23,7 @@ describe('CLI interactive', () => {
 		await runCli();
 
 		expect(prompts).toHaveBeenCalledOnce();
+		expect(mockConsoleLog).toHaveBeenCalledWith(expect.stringContaining('Feature User inserida com sucesso!'));
 
 		process.chdir(originalCwd);
 		vi.clearAllMocks();
@@ -29,6 +32,8 @@ describe('CLI interactive', () => {
 	it('finishes correctly on empty prompt', async () => {
 		const tmpProject = await createTmpProject();
 		const mockExit = vi.spyOn(process, 'exit').mockImplementation((() => {}) as (code?: string | number | null) => never);
+		const mockConsoleLog = vi.fn();
+		console.log = mockConsoleLog;
 
 		(prompts as unknown as Mock).mockResolvedValue({
 			entity: null,
@@ -39,7 +44,7 @@ describe('CLI interactive', () => {
 		await runCli();
 
 		expect(prompts).toHaveBeenCalledOnce();
-
+		expect(mockConsoleLog).toHaveBeenCalledWith(expect.stringContaining('Operação cancelada.'));
 		expect(mockExit).toHaveBeenCalledWith(0);
 	})
 });
