d28a39134279ea88ebbd32d0d0f6e7fc9afe7629
Lucas <lucas.barbosa.nas@gmail.com>
2026-01-06 14:13:46 -0300

    Gera tipos do pacote

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,22 +2,31 @@ import { defineConfig } from 'tsup';
 import { execSync } from 'node:child_process';
 import vue from 'esbuild-plugin-vue-next';
 
-export default defineConfig({
-  entry: {
-    index: 'src/common/index.ts',
-    bin: 'bin.ts',
-    cli: 'cli.ts'
+export default defineConfig([
+  {
+    entry: {
+      index: 'src/common/index.ts',
+      bin: 'bin.ts',
+      cli: 'cli.ts'
+    },
+    format: ['esm'],
+    target: 'node22',
+    outDir: 'dist',
+    clean: true,
+    splitting: false,
+    sourcemap: false,
+    esbuildPlugins: [vue()],
+    dts: false,
+    async onSuccess() {
+      execSync('cp -R src/stubs dist/stubs');
+      execSync('cp -R src/domain dist/domain');
+    }
   },
-  format: ['esm'],
-  target: 'node22',
-  outDir: 'dist',
-  clean: true,
-  splitting: false,
-  sourcemap: false,
-  esbuildPlugins: [vue()],
-  dts: false,
-  async onSuccess() {
-    execSync('cp -R src/stubs dist/stubs');
-    execSync('cp -R src/domain dist/domain');
+  {
+    entry: {
+      index: 'src/common/types.ts',
+    },
+    outDir: 'dist',
+    dts: true,
   }
-});
+]);
